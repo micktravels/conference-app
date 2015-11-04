dde9107c41611558fe7e294b7a3b78c70dfdbab3
MFink <mfink@MFinks-MacBook-Air.local>
2015-11-04 15:00:14 -0800

    Initially starting with Lesson 5 class solution Modify to remove Android ID, IOS ID

---
+++ settings.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+
+"""settings.py
+
+Udacity conference server-side Python App Engine app user settings
+
+$Id$
+
+created/forked from conference.py by wesc on 2014 may 24
+
+"""
+
+# Replace the following lines with client IDs obtained from the APIs
+# Console or Cloud Console.
+WEB_CLIENT_ID = '875880052520-vste5sntpb0jp7r3s273h79l0m6gobnj.apps.googleusercontent.com'
+
